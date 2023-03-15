c03eb4b3d145b810a7ac6d948979984085278fd0
Ivan Marinski <noobdrew23@gmail.com>
2023-03-15 15:15:03 +0200

    restart everything

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { hideInput, addProjectInput } from './projects'
-//only for dom interaction
-
-const projectInputForm = document.querySelector('.create-new-project')
-projectInputForm.addEventListener('click', hideInput(projectInputForm))
-projectInputForm.addEventListener('click', addProjectInput(projectInputForm))
